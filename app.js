e451f3d287590d650073c749ce7f74d6d2d6fc7b
Unknown <yoanybejarano@gmail.com>
2018-09-03 12:47:51 -0500

    Views

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express'),
-      consign = require('consign'),
-      morgan = require('morgan');
+    consign = require('consign'),
+    morgan = require('morgan');
 
-const app = express();
+
+var app = express();
 app.use(morgan('dev'));
 
 consign()
-      .include("config/db.js")
-      .then("models")
-      .then("libs/middlewares.js")
-      .then("routes")
-      .then("libs/boot.js")
-      .into(app);
+    .include("config/db.js")
+    .then("models")
+    .then("libs/middlewares.js")
+    .then("routes")
+    .then("libs/boot.js")
+    .into(app);
 
+var configView = require('./config/views');
+app = configView(app);
 
 /*Test*/
 // consign({verbose: false})
